438d098225f08c19919479adf14638a68345d0b2
Brian Bower <brian@brianbower.com>
2019-11-01 21:14:46 -0600

    Add Create action

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,19 @@
 class AddressesController < ApplicationController
+  before_action :exclude_admin
 
   def new
   end
-  
+
+  def create
+    user = current_user
+    address = current_user.addresses.create!(address_params)
+    redirect_to '/profile'
+  end
+
+  private
+
+  def address_params
+    params.permit(:name, :address, :city, :state, :zip, :nickname)
+  end
+
 end
