b007ba4bec2880246a94b93a49ed7a6abef704ed
Brian Bower <brian@brianbower.com>
2019-11-01 22:32:23 -0600

    Update before action and add sad path to create action

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,19 @@
 class AddressesController < ApplicationController
-  before_action :exclude_admin
+  before_action :exclude_admin_and_merchants
 
   def new
   end
 
   def create
     user = current_user
-    address = current_user.addresses.create!(address_params)
-    redirect_to '/profile'
+    address = current_user.addresses.create(address_params)
+    if address.save
+      flash[:success] = "You have successfully added an address!"
+      redirect_to '/profile'
+    else
+      flash.now[:error] = address.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   private
