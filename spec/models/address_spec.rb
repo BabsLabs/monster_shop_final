b5529cffe1b7f50310630c1fa9e29046ca9d2269
Brian Bower <brian@brianbower.com>
2019-11-02 12:11:25 -0600

    Add model method test to check if its a home address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -14,4 +14,15 @@ describe Address do
     it {should validate_uniqueness_of(:nickname).scoped_to(:user_id)}
   end
 
+  describe "model methods" do
+    it 'can tell if an address is a home address' do
+      user = User.create!(name: 'User Dude', address: '123 Fake St', city: 'Denver', state: 'CO', zip: 80008, email: ' user@user.com', password: '12345')
+      address = user.addresses.create!(address: '123 Fake St', city: 'Denver', state: 'CO', zip: 80008)
+      address_2 = user.addresses.create!(address: '456 Mountain Blvd', city: 'Aspen', state: 'CO', zip: 80345, nickname: 'Mountain House')
+
+      expect(address.not_home_address?).to be_falsy
+      expect(address_2.not_home_address?).to be_truthy
+    end
+  end
+
 end
