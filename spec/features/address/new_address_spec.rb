e81c1d27d07ffcd99f2b64f6891fd3bc66ea01d1
Brian Bower <brian@brianbower.com>
2019-11-01 22:30:13 -0600

    Add sad path tests

--- spec/features/address/new_address_spec.rb
+++ spec/features/address/new_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "As a user" do
   describe "on my user profile page" do
 
     before :each do
-      @user = User.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @user = User.create!(name: 'Cliff Hanger', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'cliff@example.com', password: 'securepassword')
       @address = @user.addresses.create!(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
     end
 
@@ -46,6 +46,76 @@ RSpec.describe "As a user" do
         expect(page).to have_content("94553")
       end
 
+    end
+
+    it "sees an error message when a new address form isn't filled out correctly" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit '/profile/addresses/new'
+
+      fill_in :address, with: ''
+      fill_in :city, with: 'Martinez'
+      fill_in :state, with: 'CA'
+      fill_in :zip, with: '94553'
+      fill_in :nickname, with: 'California House'
+      click_button 'Create Address'
+
+      expect(page).to have_content("Address can't be blank")
+
+      fill_in :address, with: '1670 Merelo Ave'
+      fill_in :city, with: 'Martinez'
+      fill_in :state, with: 'CA'
+      fill_in :zip, with: '94553'
+      fill_in :nickname, with: 'California House'
+      click_button 'Create Address'
+
+      expect(current_path).to eq('/profile')
+    end
+
+    it "shows an error message if that address is already taken" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit '/profile/addresses/new'
+
+      fill_in :address, with: '1670 Merelo Ave'
+      fill_in :city, with: 'Martinez'
+      fill_in :state, with: 'CA'
+      fill_in :zip, with: '94553'
+      fill_in :nickname, with: 'Home'
+      click_button 'Create Address'
+
+      expect(page).to have_content("Nickname has already been taken")
+    end
+  end
+end
+
+RSpec.describe "as an Admin or Merchant" do
+  it "cannot see a link to make a new address" do
+    admin = User.create!(name: 'Admin', address: '90210 Beverly Hills Blvd', city: 'Beverly Hills', state: 'CA', zip: 80999, email: 'admin@admin.com', password: 'securepassword', role: 2)
+    merchant = User.create!(name: 'Merchant', address: '1111 Shop Rd', city: 'Chicago', state: 'IL', zip: 88888, email: 'merchant@merchant.com', password: 'securepassword', role: 1)
+
+    upper_level_users = [admin, merchant]
+
+    upper_level_users.each do |admin_or_merchant|
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_or_merchant)
+
+      visit '/profile'
+
+      expect(page).to_not have_link("Add An Address")
+    end
+  end
+  it "cannot visit the page to make a new address" do
+    admin = User.create!(name: 'Admin', address: '90210 Beverly Hills Blvd', city: 'Beverly Hills', state: 'CA', zip: 80999, email: 'admin@admin.com', password: 'securepassword', role: 2)
+    merchant = User.create!(name: 'Merchant', address: '1111 Shop Rd', city: 'Chicago', state: 'IL', zip: 88888, email: 'merchant@merchant.com', password: 'securepassword', role: 1)
+
+    upper_level_users = [admin, merchant]
+
+    upper_level_users.each do |admin_or_merchant|
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_or_merchant)
+
+      visit '/profile/addresses/new'
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
